2f7c97cba7bd4b77786aac6708b9b06478b6df3a
Jeel Paradava <60537898+addy143-maker@users.noreply.github.com>
2025-01-04 01:19:59 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -216,17 +216,3 @@ $(function(){
     });
   });
 });
-
-
-window.onloadTurnstileCallback = function () {
-    turnstile.render("myWidget", {
-      sitekey: "<0x4AAAAAAA4g6H5nfu-oHs2A>",
-      callback: function (token) {
-        console.log(`Challenge Success ${token}`);
-        setTimeout(() => {
-            document.querySelector("#loader").style.display = "flex";
-            document.getElementById("myWidget").style.display = "none";
-        }, 2000);
-      },
-    });
-  };
